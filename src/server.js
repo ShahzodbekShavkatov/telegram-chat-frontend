d1e55d4cfc13449978dcaa2273f5590340774590
ShahzodbekShavkatov <shohjasurovich@gmail.com>
2022-01-18 05:59:36 +0500

    bug fixed

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
-
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
 
 
 app.listen(PORT, () => console.log("Client server is running on http://localhost:" + PORT))
